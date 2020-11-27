10cd056c1290021cebf44fd80e74f5034d804d38
nguyenle <nguyenle@nguyens-macbook-pro.home>
2020-11-27 12:23:44 +0100

    Random Movement

--- pigeon-game/src/PigeonGame.java
+++ pigeon-game/src/PigeonGame.java
@@ -20,6 +20,8 @@ public class PigeonGame extends SimpleApp {
 	//Declare pigeon image
     Image pigeon;
     int frameNumber;
+    
+    double gradient = 0;
  
     public static void main(String[] args) {
     	// TODO Auto-generated method stub
@@ -38,13 +40,30 @@ public class PigeonGame extends SimpleApp {
         screen.drawImage(bg, 0, 0);
         
         //Draw the Pigeon at random position
-        screen.drawImage(pigeon, x, y, 0, (frameNumber / 2) * 128, 192, 128);
+        screen.drawImage(pigeon,(int) x,(int) y, 0, (frameNumber / 2) * 128, 192, 128);
         /*screen.drawImage(image,
         				x-coord of where to draw pigeon on screen,y-coord of where to draw pigeon on screen,
         				x-coord of where to take it from the pic, y-coord of where to take it from the pic,
         				*each pigeon is at x-coord 0 and is 128 tall in height
         				width, height)*/
         frameNumber = (frameNumber + 1) % 18;
+        
+        int targetDestinationX = random.nextInt(400)-200;
+        while(targetDestinationX == x)
+        	targetDestinationX = random.nextInt(400)-200;
+        int targetDestinationY = random.nextInt(400)-200;
+        gradient = (targetDestinationY - y)/(targetDestinationX - x);
+        if(x < targetDestinationX) 
+        {
+        	x += 2;
+        	y += gradient;
+        }
+        else if (x > targetDestinationX) 
+        {
+        	x -= 2;
+        	y -= gradient;
+        }
+        
     }
     
     public void onMouseClick(int x, int y)
