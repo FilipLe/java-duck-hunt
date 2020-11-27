be6d3ba8a41231d0db541bb5b8cd56273d2c024d
nguyenle <nguyenle@nguyens-macbook-pro.home>
2020-11-27 12:08:49 +0100

    Pigeon start at random position

--- pigeon-game/src/PigeonGame.java
+++ pigeon-game/src/PigeonGame.java
@@ -1,11 +1,24 @@
 import java.awt.Image;
+import java.util.Random;
 
 import tbs.simpleapp.SimpleApp;
 import util.FileUtil;
 
 public class PigeonGame extends SimpleApp {
-    Image bg;
-    Image image;
+	//create random object
+	Random random = new Random();
+	
+	//Declare random x value
+	//-200 because the screen goes from -200 to 200 not 0 to 400
+	int x = random.nextInt(400) - 200;
+	
+	//Declare random y value
+	int y = random.nextInt(400) - 200;
+	
+    //Declare background image
+	Image bg;
+	//Declare pigeon image
+    Image pigeon;
     int frameNumber;
  
     public static void main(String[] args) {
@@ -14,13 +27,28 @@ public class PigeonGame extends SimpleApp {
     }
 
     public void main() {
-        bg = FileUtil.loadImage("C:/Users/lewis/Desktop/worksheets/background.png");
-        image = FileUtil.loadImage("C:/Users/lewis/Desktop/worksheets/bird.png");
+    	//Import background image
+        bg = FileUtil.loadImage("/Users/nguyenle/pigeon-game/pigeon game graphics/background.png");
+        //Import pigeon image
+        pigeon = FileUtil.loadImage("/Users/nguyenle/pigeon-game/pigeon game graphics/bird.png");
     }
     
     public void onFrame() {
+    	//Draw the Background 
         screen.drawImage(bg, 0, 0);
-        screen.drawImage(image, 0, 0, 0, (frameNumber / 2) * 128, 192, 128);
+        
+        //Draw the Pigeon at random position
+        screen.drawImage(pigeon, x, y, 0, (frameNumber / 2) * 128, 192, 128);
+        /*screen.drawImage(image,
+        				x-coord of where to draw pigeon on screen,y-coord of where to draw pigeon on screen,
+        				x-coord of where to take it from the pic, y-coord of where to take it from the pic,
+        				*each pigeon is at x-coord 0 and is 128 tall in height
+        				width, height)*/
         frameNumber = (frameNumber + 1) % 18;
+    }
+    
+    public void onMouseClick(int x, int y)
+    {
+    	
     }
 }
